3ba46b602d0dd923d3862ea86ee9ac024f02aac2
rainbowtape <loveandpeace.jin@gmail.com>
2018-07-03 03:35:19 +0100

    adding-ssl-asset

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var http = require('http');
 var https = require('https');
 var privateKey  = fs.readFileSync('./ssl/domain.key', 'utf8');
 var certificate = fs.readFileSync('./ssl/domain.crt', 'utf8');
-
+var cors = require('cors');
 var credentials = {key: privateKey, cert: certificate};
 var app = require('./app');
-
+app.use(cors());
 
 var port = 8080;
 app.set('port', port);
+
+
 
 var server = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
